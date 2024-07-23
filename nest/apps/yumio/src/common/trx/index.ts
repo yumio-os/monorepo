f94d3aa364080f1345b95878d75c63c57fb27cda
yumiodev <yumiodev@gmail.com>
2024-07-23 22:18:05 +0100

    save

--- nest/apps/yumio/src/common/trx/index.ts
+++
@@ -1,6 +0,0 @@
-import { GenericOpts } from '@yumio/common';
-import { ApiKey } from '@yumio/modules/apiKey/model';
-
-export interface Opts extends GenericOpts {
-  context?: { id?: string; apiKeyDetails?: ApiKey };
-}
--- nest/apps/yumio/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrap } from './bootstrap';
-
-bootstrap('.env.yumio')
-  .then()
-  .catch((error) => {
-    console.error(error);
-  });
